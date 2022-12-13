00bf6c8fa1719c1f13b71e54a42f7c309bbaa110
Giacomo Salato <62936640+StormyofSouth@users.noreply.github.com>
2022-12-13 15:50:04 +0100

    Create Dono.java

---
+++ babboNatale/src/main/java/com/gruppo/entities/Dono.java
@@ -0,0 +1,51 @@
+	package com.gruppo.entities;
+
+
+
+
+
+
+	import jakarta.persistence.Entity;
+
+	import jakarta.persistence.GeneratedValue;
+	import jakarta.persistence.GenerationType;
+	import jakarta.persistence.Id;
+
+	import jakarta.persistence.Table;
+
+	@Entity
+	@Table(name = "dono")
+	public class Dono {
+		
+		@Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+		private int id;
+		
+		
+		
+		private String nome;
+		private int Disponibilita;
+		
+		
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getNome() {
+			return nome;
+		}
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+		public int getDisponibilita() {
+			return Disponibilita;
+		}
+		public void setDisponibilita(int disponibilita) {
+			Disponibilita = disponibilita;
+		}
+		
+}
+
+
