25b77a57fc61747007decfc1d9b95e5ccede8fdf
Giacomo Salato <62936640+StormyofSouth@users.noreply.github.com>
2022-12-13 16:20:06 +0100

    Dono ,Bambino e dono-bambino
    creazione delle entities Dono ,Bambino e dono-bambino

--- babboNatale/src/main/java/com/gruppo/entities/Dono.java
+++ babboNatale/src/main/java/com/gruppo/entities/Dono.java
@@ -1,31 +1,43 @@
 	package com.gruppo.entities;
 
 
+	import java.util.Set;
 
-
-
-
-	import jakarta.persistence.Entity;
+import jakarta.persistence.Entity;
 
 	import jakarta.persistence.GeneratedValue;
 	import jakarta.persistence.GenerationType;
 	import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
-	import jakarta.persistence.Table;
 
 	@Entity
-	@Table(name = "dono")
 	public class Dono {
 		
 		@Id
 	    @GeneratedValue(strategy = GenerationType.IDENTITY)
 		private int id;
 		
+		@OneToMany(mappedBy="dono")
+		private Set<SaccoDono> sacchiDoni;
 		
+		@OneToMany(mappedBy="dono")
+		private Set<DonoBambino> donoAlBambino;
 		
 		private String nome;
 		private int Disponibilita;
 		
+		public Dono() {
+			super();
+		}
+
+		public Dono(int id, Set<DonoBambino> donoAlBambino, String nome, int disponibilita) {
+			super();
+			this.id = id;
+			this.donoAlBambino = donoAlBambino;
+			this.nome = nome;
+			Disponibilita = disponibilita;
+		}
 		
 		public int getId() {
 			return id;
---
+++ babboNatale/src/main/java/com/gruppo/entities/DonoBambino.java
@@ -0,0 +1,98 @@
+package com.gruppo.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+
+public class DonoBambino {
+	
+	@Id
+	private int id;
+	
+	@ManyToOne
+    @JoinColumn(name = "dono_id")
+	private Dono dono;
+
+    @ManyToOne
+    @JoinColumn(name = "bambino_id")
+    private Bambino bambino;
+    
+    
+    private LocalDateTime ora;
+    
+    private String statoConsegna;
+
+ 
+    
+	public DonoBambino() {
+		super();
+	}
+
+
+	public DonoBambino(int id, Dono dono, Bambino bambino, LocalDateTime ora, String statoConsegna) {
+		super();
+		this.id = id;
+		this.dono = dono;
+		this.bambino = bambino;
+		this.ora = ora;
+		this.statoConsegna = statoConsegna;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public Dono getDono() {
+		return dono;
+	}
+
+
+	public void setDono(Dono dono) {
+		this.dono = dono;
+	}
+
+
+	public Bambino getBambino() {
+		return bambino;
+	}
+
+
+	public void setBambino(Bambino bambino) {
+		this.bambino = bambino;
+	}
+
+
+	public LocalDateTime getOra() {
+		return ora;
+	}
+
+
+	public void setOra(LocalDateTime ora) {
+		this.ora = ora;
+	}
+
+
+	public String getStatoConsegna() {
+		return statoConsegna;
+	}
+
+
+	public void setStatoConsegna(String statoConsegna) {
+		this.statoConsegna = statoConsegna;
+	}
+    
+	
+	
+}
