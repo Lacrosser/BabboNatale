2d69b936f6c79e41608c5c3c3f6942a985efbe73
Lacrosser <114079266+Lacrosser@users.noreply.github.com>
2022-12-13 11:37:52 +0100

    Starter Set di Babbo Natale

---
+++ babboNatale/src/main/java/com/Gruppo1/BabboNataleApplication.java
@@ -0,0 +1,13 @@
+package com.Gruppo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabboNataleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabboNataleApplication.class, args);
+	}
+
+}
---
+++ babboNatale/src/test/java/com/example/Gruppo1/BabboNataleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gruppo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabboNataleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
