e5408afaa726bc121f43d952ffda8a2ce1a337ea
Lacrosser <114079266+Lacrosser@users.noreply.github.com>
2022-12-23 16:17:34 +0100

    Update BabboNataleRepo.java

--- babboNatale/src/main/java/com/gruppo/repo/BabboNataleRepo.java
+++ babboNatale/src/main/java/com/gruppo/repo/BabboNataleRepo.java
@@ -2,6 +2,7 @@ package com.gruppo.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 import com.gruppo.entities.BabboNatale;
 
 public interface BabboNataleRepo extends JpaRepository<BabboNatale, Integer> {
